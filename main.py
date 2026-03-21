cfa67eb688f58c5361e36e1421c21cb750102da3
Vismus32 <nenovandrey2@gmail.com>
2026-03-22 01:45:23 +0300

    New structure and demo-agents

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-#Будем использовать для запуска pipeline
-from agents import Segmenter, StructureBuilder, Styler, Proofreader
-from pipeline.pipeline import Pipeline
-from schemas.text import TextState
+from text_structuring.agents import Segmenter, StructureBuilder, Styler
+from text_structuring.pipeline.dag_pipeline import Pipeline
+from text_structuring.schemas.text import TextState
 
 
 pipeline = Pipeline([
     Segmenter(),
     StructureBuilder(),
-    Styler(),
-    Proofreader()
+    Styler()
 ])
 
-state = TextState("Some raw text")
+state = TextState("Это первое предложение. Это второе.")
 
 result = pipeline.run(state)
+
+print(result.styled_text)
